f202bd7356dfd542797a67ba675265684f2542db
Bunje kelly <kellybunje@gmail.com>
2023-02-16 14:23:41 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * a program that prints a line with puts function
+ * main: entry level point
+ * Return: 0(success)
+ */
+int main(void)
+{
+	puts(""Programming is like building a multilingual puzzle"/n;
+		return(0);
+		}
+
