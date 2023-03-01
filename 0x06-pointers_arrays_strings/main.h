c7eedebadcb15e7b500f91027c3331f6e6d5eacd
Bunje kelly <kellybunje@gmail.com>
2023-03-01 09:18:57 -0800

    concatenate

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file - main.h
+ * Desc: a file that contains the prototypes of all functions
+ */
+
+int _putchar(char);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+#endif
