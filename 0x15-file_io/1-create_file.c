36253d39ae6779d6332166c1ee48cf11007ca593
Bunje kelly <kellybunje@gmail.com>
2023-04-09 14:49:11 -0700

    add file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * create_file - a function that creates a file
@@ -12,22 +13,26 @@ int create_file(const char *filename, char *text_content)
 {
 	int o;
 	int w;
-	int len = 0;
+
 
 	if (filename == NULL)
 		return (-1);
+
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (o == -1)
+		return (-1);
+
 	if (text_content == NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		w = write(o, text_content, strlen(text_content));
+
+		if (w == -1)
+		{
+			close(o);
+			return (-1);
+		}
 	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	w = write(o, text_content, len);
-
-	if (w == -1)
-		return (-1);
 
 	close(o);
 
