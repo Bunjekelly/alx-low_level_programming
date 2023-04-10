f34b6467f5077ef0a2e69beceeae782bf932edb9
Bunje kelly <kellybunje@gmail.com>
2023-04-10 04:00:30 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,30 +11,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o;
-	int w;
+	int fo;
+	int fw;
+	int len = 0;
 
 
 	if (filename == NULL)
 		return (-1);
 
-	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fo = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (o == -1)
+	if (fo == -1)
 		return (-1);
 
 	if (text_content == NULL)
-	{
-		w = write(o, text_content, strlen(text_content));
+		text_content = "";
 
-		if (w == -1)
-		{
-			close(o);
-			return (-1);
-		}
-	}
+	for (len = 0; text_content[len]; len++)
+		;
 
-	close(o);
+
+	fw = write(fo, text_content, strlen(text_content));
+
+	if (fw == -1)
+		return (-1);
+
+	close(fo);
 
 	return (1);
 }
