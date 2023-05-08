18b6aff3c20a1ae98ebb2425f34ac4efa422dd84
Bunje Kelly <kellybunje@gmail.com>
2023-05-08 04:53:11 -0700

    a function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: name of the file to create
+ * @text_content: pointer to NULL terminator string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fo;
+	int fw;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	fw = write(fo, text_content, len);
+
+	if (fw == -1)
+		return (-1);
+
+	close(fo);
+
+	return (1);
+}
