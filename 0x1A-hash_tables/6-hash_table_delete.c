bae08d1238438ed7a648697be16ceced0f48fb85
Bunje Kelly <kellybunje@gmail.com>
2023-07-07 15:16:54 -0700

    corrections

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
  * hash_table_delete - deletes a hash table
@@ -18,7 +19,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (k = 0; k < ht->size; k++)
 	{
 		temp1 = ht->array[k];
-		while (temp1 != NULL)
+		while ((temp1 = temp2) != NULL)
 		{
 			temp1 = temp1->next;
 			free(temp2->key);
