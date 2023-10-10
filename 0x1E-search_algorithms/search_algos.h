d7bcd6aef1467d041c35293e6f5ca1ace413ec49
Bunje Kelly <kellybunje@gmail.com>
2023-10-10 10:59:24 -0700

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,11 +3,6 @@
 #include <stddef.h>
 #include <stdio.h>
 
-/**
- * file - header
- * desc: contains all prototypes
- */
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
